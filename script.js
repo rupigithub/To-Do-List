586ea259e39df0fe12f7d85ef49fc79a2ca2b794
rupigithub <singhrupika4@gmail.com>
2024-02-04 21:57:41 +0530

    first-commit

---
+++ script.js
@@ -0,0 +1,9 @@
+let date = document.getElementById("input-date");
+let priority = document.getElementById("high-priority");
+
+let toggleInput = document.getElementsByClassName("created-text");
+toggleInput.addEventListener("click", giveInputs);
+function giveInputs(){
+
+priority.classList.show;
+}
